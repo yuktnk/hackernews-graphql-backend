d46eb2179013f2655b58c3daa35d90f8bb342b7e
yuktnk <yuki0wl@gmail.com>
2023-05-01 21:37:48 +0900

    Prismaいろいろじゅんび

---
+++ src/script.js
@@ -0,0 +1,25 @@
+// データベースにアクセスするためのクライアントライブラリ
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const newLink = await prisma.link.create({
+        data: {
+            description: "テストテストテスト",
+            url: "https://www.google.com/",
+        },
+    });
+
+    const allLinks = await prisma.link.findMany();
+    console.log(allLinks);
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        // データベース接続を閉じる
+        prisma.$disconnect;
+    });
