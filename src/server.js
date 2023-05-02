3683883127281926a30763d3b9393808948a22bb
yuktnk <yuki0wl@gmail.com>
2023-05-02 15:51:58 +0900

    Psotのリゾルバ

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const { PrismaClient } = require("@prisma/client");
+const { getUserId } = require("./utils");
 
 const prisma = new PrismaClient();
 
@@ -31,8 +32,12 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
     resolvers,
-    context: {
-        prisma,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId: req && req.headers.authorization ? getUserId(req) : null,
+        };
     },
     // csrfPrevention: true,
 });
