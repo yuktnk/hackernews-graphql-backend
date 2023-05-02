664c7f7554260ab9a5f6a77af03d61a0f037ba8c
yuktnk <yuki0wl@gmail.com>
2023-05-02 10:20:38 +0900

    serverとPrismaの連携

--- src/server.js
+++ src/server.js
@@ -2,35 +2,28 @@ const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-// HackerNewsの1つ1つの投稿
-let links = [
-    {
-        id: "link-0",
-        description: "テストテストテストテストテストテスト",
-        url: "https://www.google.com/",
-    },
-];
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 // リゾルバ関数
 const resolvers = {
     Query: {
         info: () => "Hacker News クローン",
-        feed: () => links,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany();
+        },
     },
 
     Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length;
-
-            const link = {
-                id: `line-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-
-            links.push(link);
-
-            return link;
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            });
+            return newLink;
         },
     },
 };
@@ -38,6 +31,9 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
     resolvers,
+    context: {
+        prisma,
+    },
     // csrfPrevention: true,
 });
 
